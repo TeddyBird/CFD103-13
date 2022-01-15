24e6e7d40ad003a9a14de56d6d7d3df5defa145a
TeddyBird <ted850416@gmail.com>
2022-01-15 16:41:20 +0800

    新增購物車頁面及購物車功能

--- JS/product-content.js
+++ JS/product-content.js
@@ -10,39 +10,6 @@ function closeNutrition(){
     document.querySelector(".nutrition").style.display="none";
 }
 
-
-function clickMinus(){
-    let buyNum = document.querySelector(".counter-bar input").value;
-    if(buyNum == 1){
-        document.querySelector(".counter-bar input").value = 1;
-    }else{
-        buyNum--;
-        document.querySelector(".counter-bar input").value = buyNum;
-    }
-}
-function clickPlus(){
-    let buyNum = document.querySelector(".counter-bar input").value;
-    buyNum++;
-    document.querySelector(".counter-bar input").value = buyNum;
-}
-
-
-function addCart(){
-    let buyNum = document.querySelector(".counter-bar input").value;
-    let shopcartAlert = document.querySelector(".shopcart-alert");
-    alert("已加入購物車");
-    buyNum = 1;
-    document.querySelector(".counter-bar input").value = buyNum;
-    shopcartAlert.style.display="block";
-    shopcartAlert.innerText = parseInt(shopcartAlert.innerText) + 1; 
-}
-
-function  clickCart(){
-    let shopcartAlert = document.querySelector(".shopcart-alert");
-    shopcartAlert.style.display="none";
-    shopcartAlert.innerText = 0; 
-}
-
 function init(){
     let imgs = document.querySelectorAll(".switch-pic img")
     for(let i=0; i<imgs.length; i++){
@@ -50,9 +17,9 @@ function init(){
     }
     document.querySelector(".nutrition-btn").onclick = showNutrition;
     document.querySelector(".close-nutrition").onclick = closeNutrition;
-    document.querySelector(".minus-btn").onclick = clickMinus;
-    document.querySelector(".plus-btn").onclick = clickPlus;
-    document.querySelector(".buybtn").onclick = addCart;
+    // document.querySelector(".minus-btn").onclick = clickMinus;
+    // document.querySelector(".plus-btn").onclick = clickPlus;
+    // document.querySelector(".buybtn").onclick = addCart;
     document.querySelector(".shopcart-icon").onclick = clickCart;
 }
 window.addEventListener("load",init,false);
